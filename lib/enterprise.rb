dd8721fa58d644ffb4e81c872ff88e277f9cfcc3
Aaron Patterson <aaron.patterson@gmail.com>
2009-06-17 17:33:07 -0700

    more entry to the enterprise

--- lib/enterprise.rb
+++ lib/enterprise.rb
@@ -3,9 +3,9 @@ require 'ruby_parser'
 require 'ruby2ruby'
 require 'polyglot'
 
-require 'enterprise/sexp_hacks'
 require 'enterprise/sexml'
 require 'enterprise/file_loader'
+require 'enterprise/sexp_hacks'
 
 module Enterprise
   VERSION = '1.0.0'
